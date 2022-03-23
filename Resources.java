f70c6c5b679d07b033092b5a7b6dc822fff112b6
RahmaFares <rwalid.nes@gmail.com>
2022-03-24 00:08:51 +0100

    Resources class is edited

--- Resources.java
+++ Resources.java
@@ -1,5 +1,22 @@
-
 public class Resources {
+    String Name; //an attribute for the name of the resource
+    public Resources (){}
+    public Resources(String name)
+    {
+        this.Name = name;
+    }
+
+    public String getResourceName()
+    {
+        System.out.println("This is getResourceName() of Resources");
+        return Name;
+    }
+
+    public Asteroid reactWithSun(Asteroid a){
+        System.out.println("This is reactWithSun() of Resources");
+
+        return a;
+    }
 
 }
 
