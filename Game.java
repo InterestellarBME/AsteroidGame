54624a647dbb7500ac6838caca440885bfadbefc
Roqaya Alhalawany <rokaya.alhalawany@gmail.com>
2022-03-24 00:08:23 +0100

    adding game class

---
+++ Game.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.System;
+public class Game
+{
+
+    public bool CheckWin;
+    public void Start()
+    {
+        System.out.printIn("This is Start() of Game");
+    }
+
+    public void End()
+    {
+        System.out.printIn("This is End() of Game");
+    }
+
+    public Traveller RemoveTraveller(t Traveller)
+    {
+        System.out.printIn("This is RemoveTraveller() of Game");
+        return null;
+    }
+    public Traveller AddTraveller (t Traveller)
+    {
+        System.out.printIn("This is AddTraveller() of Game");
+        return null;
+    }
+
+
+}
