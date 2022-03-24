82174b84f9ec2be4057a3d439d6a3d958550fb38
Basit <abdulbasit8868@outlook.com>
2022-03-24 01:08:00 +0100

    basit part

---
+++ Robot.java
@@ -0,0 +1,13 @@
+
+public class Robot extends Traveller{
+
+    private int livesLost;
+
+
+    public void die()
+    {
+            System.out.println("This is the die() function of Robot class");
+    }
+
+
+}
