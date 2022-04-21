572ba44d6a16360d9c8e2328e3b8dcba4cfac6c0
Mohammaderfan koupaei <82345513+eerrffuunn@users.noreply.github.com>
2022-04-21 02:07:05 +0200

    new version

--- Asteroid.java
+++ Asteroid.java
@@ -1,221 +1,160 @@
-import javax.swing.*;
-
-import java.net.SocketOption;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-import static java.lang.Math.random;
-
-public class Asteroid implements IPlace{
-
-    //resourcename represent of integers from 0 to 3
-
-    public int depth;
-    public int location;
-    public boolean isTeleportable;
-    public boolean checkDrilled;
-    public boolean isHollow;
-    public  boolean isRadioactive;
-    public Resources resources;
-    public int random;
-   // public Resources res;
-
-
-    /**
-     * Setting the depth to a random value between 7 and 20.
-     * Setting isTeleportable and checkDrilled initially to false.
-     * Using method Random().nextInt with two values 1 and 2,
-     * so that if the value is 2 the asteroid is randomly generated as a hollow one.
-     * The Switch method is used to deterimne which resource is contained in the asteroid.
-     */
-    public Asteroid()
-    {
-        System.out.println("This is Asteroid() of Asteroid");
-       // this.res=resources;
-        this.depth=new Random().nextInt(20)+7;
-        this.location=location;
-        this.isTeleportable=false;
-        this.checkDrilled=false;
-        // 0: hollow, 1: has resources
-        int ran = new Random().nextInt(2);
-        if (ran == 1)
-        this.isHollow=false;
-        else {
-            this.isHollow = true;
-            return;
-        }
-        this.isRadioactive=false;
-        if(isHollow == false)
-        resources =  Asteroid.addResources();
-        switch (resources.getNumber()){
-            case 1:
-            {
-                this.isHollow=false;
-                this.isRadioactive=false;
-            }
-            break;
-            case 2:
-            {
-                this.isHollow=false;
-                this.isRadioactive=false;
-            }
-            break;
-            case 3:
-            {
-                this.isHollow=false;
-                this.isRadioactive=true;
-            }
-            break;
-        }
-        // calls for the skeleton test
-        this.getDepth();
-    }
-
-    /**
-     * This function generates a random resource based on the number gemerated by Random() function
-     * @return the randomly generated resource.
-     */
-    public static Resources addResources()
-    {
-        System.out.println("This is addResource() of Asteroid");
-        int random=new Random().nextInt(3)+1;
-        switch (random)
-        {
-            case 1:
-            {
-                return new Carbon();
-            }
-            case 2:
-            {
-                return new Iron();
-            }
-            case 3: {
-                return new Uranium();
-            }
-            default: return null;
-        }
-    }
-
-    /**
-     * This function returns an integer to show lenght of asteroids mantle
-     * @return asteroid depth.
-     */
-    public  int getDepth(){
-        System.out.println("This is getDepth() of Asteroid");
-        return  this.depth;
-
-    }
-
-    /**
-     *
-     * @return location of the asteroid.
-     */
-    public int getLocation(){
-        return this.location;
-    }
-
-    /**
-     *
-     * @return name of the resouce.
-     */
-    public String getResourceName(){
-        System.out.println("This is getResourceName() of Asteroid");
-       return resources.getResourcesName();
-    }
-
-    /**
-     * This function decreases the depth by one unit after every drilloperattion that happens untill it becomes a hollow asteroid.
-     */
-    public void decreasedepth(){
-
-        System.out.println("this method decreases depth after each drill action call");
-    /*
-    if(depth!=0)
-    {
-        System.out.println("Decreasing the depth of the the current Asteroid!");
-        depth--;
-    }
-    else{
-        System.out.println("Cannot decrease depth any further. It's time to mine!");
-    }*/
-    }
-
-    /**
-     * This function is called when the game starts to add random resource types to the asteroids.
-     */
-    public  void  addresource()
-    {
-/*
-randomly passing resources to each asteroid
-
+/**
+ * Represents an Asteroid. Asteroid are the main object. They form the asteroid belt the Travellers are moving through.
+ * Asteroids can be drilled and the Resource in their cores can be mined.
+ * Asteroids with radioactive cores can explode. Hollow Asteroids offer protection from sunstrom.
+ * @author InProgress
  */
+public class Asteroid {
 
-        System.out.println("this method adds resources");
+    //<editor-fold desc="Attributes">
+
+ 
+    private int Depth;
+    private String name;
+    public Gate gate;
+    private ArrayList<Settler> settlersOnAsteroid;
+    private ArrayList<Robot> robotsOnAsteroid;
+    private ArrayList<Resources> resourceOfAsteroid;
+    private ArrayList<Resources> storedResourceOfAsteroid;
+    private Boolean isAtPerihelion;
+    private Boolean isHollow;
+    private Boolean isRadioactive;
+    private Boolean hasGate;
+    private Boolean isExploded;
+
+    //</editor-fold>
+
+
+    //<editor-fold desc="Constructor">
+
+
+    public Asteroid(int rnd) {
+       
+       
+        this.Depth = new Random().nextInt(10)+7; // random number 0=<Depth<10
+        this.isAtPerihelion = false;
+        this.resourceOfAsteroid = new ArrayList<>();
+
+        switch (rnd) { // switch case to determine which resource is assigned
+            case 1: { // Assigns Carbon to this Asteroid
+                this.resourceOfAsteroid.add(new Carbon());
+                this.isHollow = false;
+                this.isRadioactive = false;
+            } break;
+            case 2: { // Assigns Iron to this Asteroid
+                this.resourceOfAsteroid.add(new Iron());
+                this.isHollow = false;
+                this.isRadioactive = false;
+            }break;
+            case 3: { // Assigns Uranium to this Asteroid
+                this.resourceOfAsteroid.add(new Uranium());
+                this.isHollow = false;
+                this.isRadioactive = true;
+            }break;
+            case 4: { // Assigns WaterIce to this Asteroid
+                this.resourceOfAsteroid.add(new WaterIce());
+                this.isHollow = false;
+                this.isRadioactive = false;
+            }break;
+           
+            
+        }
+
+        this.hasGate = false; // initialized as false.
+        this.isExploded = false; // initialized as false.
+        this.settlersOnAsteroid = new ArrayList<>();
+        this.robotsOnAsteroid = new ArrayList<>();
+        this.storedResourceOfAsteroid = new ArrayList<>();
     }
-    // was called changeToHollow() in the class diagram
 
-    /**
-     * This function changes the asteroid into a hollow one after it becomes fully mined.
-     */
-    public void makeEmpty()
-    {
-        this.resources=null;
-        this.depth=0;
-        this.isHollow=true;
-        this.isRadioactive=false;
 
-    }
-
-    /**
-     * This function checks if depth of drilled is equal to the asteroid depth
-     * so then the asteroid can change to hollow.
-     */
-    public void fullyMined()
-    {
-        System.out.println("this method shows when depth is 0");
+    public int getDepth() {
+        return this.Depth;
     }
 
 
-    /**
-     *
-     * @return random boolean value
-     */
-    public boolean checkPrehilion()
-    {
-        return new Random().nextBoolean();
+    public void decreasedepth() {
+
+        this.Depth--;
     }
 
-    /**
-     * This function is going to be overridden every time the location the setller changes.
-     * @return null
-     */
-    @Override
-    public IPlace getNeighbour() {
-        System.out.println("This is getNeighbour of Asteroid - IPlace");
-        return null;
+
+    public void fullyMined() {
+        this.resourceOfAsteroid.clear(); // Removes the resource of this Asteroid.
+        this.isHollow = true; // This Asteroid is hollow.
+        this.isRadioactive = false; // A hollow Asteroid cannot be radioactive.
     }
 
-    /**
-     * This function adds the neighbor of the gate.
-     */
-    @Override
-    public void addNeighbour() {
-        System.out.println("This is addNeighbour of Asteroid - IPlace");
+
+    public void goToPrehilion(Boolean atPerihelion) {
+        isAtPerihelion = atPerihelion;
+        if(isAtPerihelion && isRadioactive && Depth == 0) // checks if the Resource is Uranium and if it has to explode
+        {
+            this.resourceOfAsteroid.get(0).explode(this);
+        }
+        if(isAtPerihelion && this.getResourceOfAsteroid().get(0) instanceof WaterIce) // checks if the resource is WaterIce and if it has to sublime
+        {
+            this.resourceOfAsteroid.get(0).sublime(this);
+        }
     }
 
-    /**
-     * This function removes the neighbor of the gate and gets overridden everytime this needs to be happened.
-     */
-    @Override
-    public void deleteNeighbour() {
-        System.out.println("This is deleteNeighbour of Asteroid - IPlace");
+    public void hideTravellers() {
+        for (Settler settler : settlersOnAsteroid ) {
+            if(!settler.checkHide()) {
+                settler.hide(this);
+            }
+        }
+        for (Robot robot : robotsOnAsteroid ) {
+            if(!robot.checkHide()) {
+                robot.hide(this);
+            }
+        }
     }
 
-    /**
-     * connects a settler or a robot to a gate or an asteroid
-     */
-    @Override
-    public void addTraveller() {
-        System.out.println("This is addTraveller of Asteroid - IPlace");
+    public Gate getGate() { return gate; }
+    public void setGate(Gate gate) { this.gate = gate;  }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public void setDepth(int Depth) { this.Depth = Depth; }
+
+    public Boolean getAtPerihelion() { return isAtPerihelion; }
+    public void setAtPerihelion(Boolean atPerihelion) {
+        isAtPerihelion = atPerihelion;
     }
+
+
+    public Boolean getHollow() { return isHollow; }
+    public void setHollow(Boolean hollow) {isHollow = hollow;}
+
+
+    public Boolean getRadioactive() { return isRadioactive; }
+    public void setRadioactive(Boolean radioactive) { isRadioactive = radioactive; }
+
+    public Boolean getHasGate() { return hasGate; }
+    public void setHasGate(Boolean hasGate) { this.hasGate = hasGate; }
+
+    public List<Settler> getSettlersOnAsteroid() { return this.settlersOnAsteroid; }
+    public void setSettlersOnAsteroid(Settler newSettler) { this.settlersOnAsteroid.add(newSettler); }
+
+    public List<Robot> getRobotsOnAsteroid() { return this.robotsOnAsteroid; }
+    public void setRobotsOnAsteroid(Robot newRobot) { this.robotsOnAsteroid.add(newRobot); }
+
+    public List<Resources> getStoredResourceOfAsteroid() { return storedResourceOfAsteroid; }
+    public void setStoredResourceOfAsteroid(Resources newResource) { this.storedResourceOfAsteroid.add(newResource); }
+
+    public List<Resources> getResourceOfAsteroid() { return resourceOfAsteroid; }
+    public void setResourceOfAsteroid(Resources res) {
+        this.resourceOfAsteroid.clear();
+        this.resourceOfAsteroid.add(res);
+    }
+
+    public Boolean getExploded() { return isExploded; }
+    public void setExploded(Boolean exploded) { isExploded = exploded; }
+
 }
